51b7440a1fa9e726f191f9eee811026c08b25e8b
JaymieCruz <jaymielyncruz@gmail.com>
2024-12-06 19:52:38 +0800

    Add files via upload

---
+++ Task sheet 1-1-2/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num2 = 3110;
+        byte zero = 0;
+        char myChar = 'r';
+        short one = 1;
+        float myFloat = 2.0f;
+        boolean myBoolean = true;
+
+        String output = "H" + num2 + " w" + zero+ myChar + one + "d " + myFloat + " " + myBoolean;
+        System.out.println(output);
+    }
+}
