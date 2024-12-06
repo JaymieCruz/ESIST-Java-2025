def0bdc051e786c215f708d5ff89cec1ed2e7d62
JaymieCruz <jaymielyncruz@gmail.com>
2024-12-06 20:11:51 +0800

    Add files via upload

---
+++ Tasksheet112.java
@@ -0,0 +1,13 @@
+public class Tasksheet112 {
+    public static void main(String[] args) {
+        int num1 = 3110;
+        byte zero = 0;
+        char Char = 'r';
+        short one = 1;
+        float Float = 2.0f;
+        boolean Bool = true;
+
+        String output = "H" + num1 + " w" + zero + Char + one + "d " + Float + " " + Bool;
+        System.out.println(output);
+    }
+}
