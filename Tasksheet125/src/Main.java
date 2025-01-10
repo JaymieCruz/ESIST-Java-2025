01823fa0ac4e6974669176de497e0500e1244e1a
JaymieCruz <jaymielyncruz@gmail.com>
2025-01-10 16:51:43 +0800

    Add files via upload

---
+++ Tasksheet125/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Camry", 2022, 4);
+
+        car.displayDetails();
+    }
+}
---
+++ Tasksheet125/src/Tasksheet125.java
@@ -0,0 +1,15 @@
+public class Tasksheet125 {
+
+    String make;
+    String model;
+    int year;
+
+    public Tasksheet125(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+
+}
+
